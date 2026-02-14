c9481ddff9c4a5f7c34006e7c29e30be52c5eb16
Tania Koh Tze Ern <taniakohtzeern@gmail.com>
2026-02-14 14:24:25 +0800

    edited config

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: `${process.env.BACKEND_URL || "http://localhost:8000"}/api/:path*`,
-      },
-    ];
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
